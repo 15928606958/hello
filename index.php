2f42ac7ab1a4a214f328b7806fc9da716e5a0525
15928606958 <173954283@qq.com>
2017-07-12 20:34:45 +0800

    添加了一段Html代码
    添加了一段Html代码

--- index.php
+++ index.php
@@ -5,3 +5,16 @@
  * Date: 2017/7/12
  * Time: 19:26
  */
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>测试</title>
+    </head>
+    <body>
+        <body>
+            <h1>hello world</h1>
+        </body>
+    </body>
+</html>
